ac4095c5a5313930122a308b2d7c3fb0c0e9d281
sentryGun53 <sentryGun53@gmail.com>
2017-04-25 14:54:50 -0500

    write_ppm.cpp file

---
+++ write_ppm.cpp
@@ -0,0 +1,22 @@
+/* Write ppm files */
+
+void write_ppm(unsigned char *image, const char *filename, int width, int height, int size) {
+    std::ofstream outFile (filename,  std::ios::binary);
+    outFile << "P6\n" << width << " " << height << " 255\n";
+    outFile.write (( char* )image, size);
+    cout << "PPM image saved at " << filename << "  - " << width << " x " << height << endl;
+}
+
+void write_ppm_greyscale(unsigned char *image, const char *filename, int width, int height, int size) {
+    std::ofstream outFile (filename,  std::ios::binary);
+    outFile << "P6\n" << width << " " << height << " 255\n";
+    for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+        	char val = image[row * width + col];
+    		outFile.write (&val, 1);
+    		outFile.write (&val, 1);
+    		outFile.write (&val, 1);
+    	}
+    }
+    cout << "PPM image saved at " << filename << "  - " << width << " x " << height << endl;
+}
