d9c026e41cb7ee9f2bad2a76b50639cbd1801c21
sentryGun53 <sentryGun53@gmail.com>
2017-04-25 15:09:20 -0500

    make test

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 main:
 	g++ main.cpp -o main -I/usr/local/include -lraspicam -lmmal -lmmal_core -lmmal_util -L/opt/vc/lib
+
+test:
+	g++ test.cpp -o test -I/usr/local/include -lraspicam -lmmal -lmmal_core -lmmal_util -L/opt/vc/lib
---
+++ test.cpp
@@ -0,0 +1,83 @@
+/*
+
+MAKE:
+  g++ main.cpp -o main -I/usr/local/include -lraspicam -lmmal -lmmal_core -lmmal_util -L/opt/vc/lib
+
+TODO:
+  - write output back into input array in background subtration to save some space
+  - frame_thresh (results from bg subtraction) only needs to be binary (24x smaller)
+
+TEST HARNESS:
+  - grab background frame and a new frame
+  - feed same frame & background to both algorithms (sequential & parallel)
+  - time execution
+  - compare results
+  - possibly test at different resolutions?
+
+*/
+
+#include <ctime>
+#include <unistd.h>
+#include <fstream>
+#include <iostream>
+#include <raspicam/raspicam.h>
+using namespace std;
+
+#include "write_ppm.cpp"
+#include "sequential.cpp"
+
+#define FRAME_WIDTH   320
+#define FRAME_HEIGHT  240
+
+int main (int argc,  char **argv) {
+
+    // initialize camera object
+    raspicam::RaspiCam Camera;
+
+    // write camera parameters (resolution, format, white balance, etc)
+    Camera.setFormat(raspicam::RASPICAM_FORMAT_RGB);
+    Camera.setCaptureSize(FRAME_WIDTH, FRAME_HEIGHT);
+
+    // calculate size of image buffers
+    int img_size_bytes = Camera.getImageTypeSize(Camera.getFormat());
+
+    // setup buffers for image data
+    unsigned char *background = new unsigned char[img_size_bytes]();
+    unsigned char *frame_raw  = new unsigned char[img_size_bytes]();
+
+    // open connection to camera
+    cout << "Opening Camera..." << endl;
+    if (!Camera.open()) {
+        cerr << "Error opening camera" << endl;
+        return -1;
+    }
+
+    // wait a while until camera stabilizes
+    cout << "Sleeping for 3 secs" << endl;
+    sleep(3);
+
+    // record background
+    Camera.grab();
+    Camera.retrieve(background);
+    cout << "Recorded background." << endl;
+    write_ppm(background, "background.ppm", FRAME_WIDTH, FRAME_HEIGHT, img_size_bytes);
+
+    //for (int i = 0; i < 10; i++) {
+        // capture
+        Camera.grab();
+        // extract the image in rgb format
+        Camera.retrieve(frame_raw);
+        write_ppm(frame_raw, "raw.ppm", FRAME_WIDTH, FRAME_HEIGHT, img_size_bytes);
+
+        // TODO timing
+        seq(img_size_bytes, FRAME_WIDTH, FRAME_HEIGHT, background, frame_raw);
+
+        // TODO timing
+        //par(img_size_bytes, FRAME_WIDTH, FRAME_HEIGHT, background, frame_raw);
+    //}
+
+    // free resrources
+    delete background;
+    delete frame_raw;
+    return 0;
+}
