2bd772c37ebd320c7b0d31d3db7444783628b207
sentryGun53 <sentryGun53@gmail.com>
2017-05-11 10:53:07 -0500

    4 threads for blur

--- blur_par.cpp
+++ blur_par.cpp
@@ -68,24 +68,57 @@ void *blur_thread_start(void *thread_arg) {
 
 void blur_parallel (unsigned char *res, unsigned char *frame, unsigned char *blur_vector,
         int frame_width, int frame_height, int blur_width, int blur_height) {
-    pthread_t thread_id;
-    blur_args args;
+    pthread_t thread_id1;
+    pthread_t thread_id2;
+    pthread_t thread_id3;
 
-    args.res = res;
-    args.frame = frame;
-    args.blur_vector = blur_vector;
-    args.frame_width = frame_width;
-    args.frame_height = frame_height;
-    args.blur_width = blur_width;
-    args.blur_height = blur_height;
-    args.start_row = 0;
-    args.end_row = frame_height / 2;
-    args.start_col = 0;
-    args.end_col = frame_width;
+    blur_args args1;
+    args1.res = res;
+    args1.frame = frame;
+    args1.blur_vector = blur_vector;
+    args1.frame_width = frame_width;
+    args1.frame_height = frame_height;
+    args1.blur_width = blur_width;
+    args1.blur_height = blur_height;
+    args1.start_row = 0;
+    args1.end_row = frame_height / 4;
+    args1.start_col = 0;
+    args1.end_col = frame_width;
+    blur_args args2;
+    args2.res = res;
+    args2.frame = frame;
+    args2.blur_vector = blur_vector;
+    args2.frame_width = frame_width;
+    args2.frame_height = frame_height;
+    args2.blur_width = blur_width;
+    args2.blur_height = blur_height;
+    args2.start_row = frame_height / 4;
+    args2.end_row = frame_height / 2;
+    args2.start_col = 0;
+    args2.end_col = frame_width;
+    blur_args args3;
+    args3.res = res;
+    args3.frame = frame;
+    args3.blur_vector = blur_vector;
+    args3.frame_width = frame_width;
+    args3.frame_height = frame_height;
+    args3.blur_width = blur_width;
+    args3.blur_height = blur_height;
+    args3.start_row = frame_height / 2;
+    args3.end_row = (frame_height / 4) * 3;
+    args3.start_col = 0;
+    args3.end_col = frame_width;
 
-    pthread_create(&thread_id, NULL, blur_thread_start, &args);
+    pthread_create(&thread_id1, NULL, blur_thread_start, &args1);
+    pthread_create(&thread_id2, NULL, blur_thread_start, &args2);
+    pthread_create(&thread_id3, NULL, blur_thread_start, &args3);
+
     blur_helper (res, frame, blur_vector, frame_width, frame_height,
-            blur_width, blur_height, frame_height/2, frame_height, 0, frame_width);
-    pthread_join(thread_id, NULL);
+            blur_width, blur_height, (frame_height/4)*3, frame_height,
+            0, frame_width);
+
+    pthread_join(thread_id1, NULL);
+    pthread_join(thread_id2, NULL);
+    pthread_join(thread_id3, NULL);
 }
 
